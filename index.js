70425f45f58ada31d32edc0c15fb52444bea03da
Richard To <gohn76@gmail.com>
2012-08-09 22:10:58 -0800

    Add auth, make doc-md middleware, upate styles

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-exports.doc_md = function(config){
+var server = function(config){
 
+	var path = require('path')
+	var _ = require('underscore')	
+	var express = require('express')
+	var auth = require('connect-auth')
+
+	var app = express();
+
+	var settings = {
+		doc_md: {},
+		static_path: path.join(__dirname, 'public'),
+		view_path: path.join(__dirname, 'views'),
+		auth: {
+			name: 'never',
+			strategy: auth.Never(),
+			cookie_secret: 'secret'
+		},
+	}
+	_.extend(settings, config)
+
+	app.engine('html', require('ejs').renderFile)
+
+	app.use(express.static(settings.static_path))
+		.use(express.cookieParser(settings.auth.cookie_secret))
+   		.use(express.session())
+   		.use(express.bodyParser())		
+		.use(auth({strategies: [settings.auth.strategy]}))
+		.use(auth_middleware(settings.auth.name))
+		.use(doc_md(settings.doc_md))
+
+	app.set('views', settings.view_path)
+	app.set('view engine', 'html')
+
+	app.get('/*', function(req, res){
+	    res.send(404)
+	})
+
+	return app
+}
+
+var doc_md = function(config){
+	
 	var async = require('async')
 	var lazy = require("lazy")
 	var path = require('path')
 	var fs = require('fs')
 	var _ = require('underscore')	
 	var mmd = require('multimarkdown')
-	var express = require('express')
-	var app = express();
 
 	var settings = {
-		toc_file: null,
-		md_path: null,
+		doc_path: null,
+		toc_filename: 'toc.json',
 		ext: '.md',
-		title: 'Doc Md',
+		title: 'Doc-Md',
 		default_page: 'index',
+		base_url: '',
 		theme: {
-			template: 'default',
-			static_path: express.static(path.join(__dirname, 'public')),
-			view_path: path.join(__dirname, 'views'),
-			css: '/css/style.css'
+			template: 'doc-md',
+			css: '/css/doc-md.css'
 		}
 	}
-
 	_.extend(settings, config)
 
-	var gen_toc = function(toc_file, func){
+	var parse_toc = function(toc_file, func){
+		var trim_regex = /^\s*|\s*$/g
+
 		fs.readFile(toc_file, function(err, data){
 			if(err == null){
 				toc = JSON.parse(data)
@@ -35,21 +74,22 @@ exports.doc_md = function(config){
 					toc, 
 					function(filename, callback){
 						var file_path = path.join(
-						settings.md_path, filename + settings.ext)
+						settings.doc_path, filename + settings.ext)
 						meta[filename] = {
 							title: filename, 
-							subheaders: []
+							subheads: []
 						}
 						new lazy(fs.createReadStream(file_path))
 							.lines
 							.map(String)
 							.forEach(function(line){
 								if(line[0] == '#' && line[1] != '#'){
-									meta[filename].title = line.replace(/^#\s*|\s*$/g, '')
+									meta[filename].title = line.substring(1).replace(trim_regex, '')
 								} else if(line[0] == '#' && line[1] == '#' && line[2] != '#'){
-									meta[filename].subheaders.push({ 
-										title: line.replace(/^##\s*|\s*$/g, ''),
-										anchor: line.replace(/^##\s*|\s*$/g, '').replace(/ /g, '').toLowerCase()
+									var subhead = line.substring(2).replace(trim_regex, '')
+									meta[filename].subheads.push({ 
+										title:subhead,
+										anchor: subhead.replace(/ /g, '').toLowerCase()
 									})
 								}
 
@@ -61,53 +101,99 @@ exports.doc_md = function(config){
 					function(err, results){
 						func(err, toc, meta)
 					}
-				);
+				)
 			} else {
-				res.send(404)
+				console.log(err);
+				func("Invalid toc file.")
 			}		
 		})
+	}	
+
+	var build_toc = function(toc, meta){
+		var toc_str = ''
+
+		var count_pages = toc.length		
+		for(var i = 0; i < count_pages; i++){
+			var page =  meta[toc[i]]
+			var subheads = page['subheads']
+			
+			toc_str += '- [' + page.title + "] (" + settings.base_url + "/" + toc[i] + ")\n"
+			
+			var count_subheads = subheads.length
+			for(var g = 0; g < count_subheads; g++){
+				toc_str += '    - [' + 
+					subheads[g].title + "] (" + 
+					settings.base_url + "/" + toc[i] + "#" + 
+					subheads[g].anchor + ")\n"
+			}
+		}
+		return toc_str
 	}
-	
-	gen_toc(settings.toc_file, function(err, toc, meta){
-		console.log(meta['index']['subheaders'][0].anchor)
-	})
 
-	app.engine('html', require('ejs').renderFile)
+	return function(req, res, next){
+		var base_url = settings.base_url.replace('/', '\/')
+		var url_regex =  new RegExp("^" + base_url + "\/([a-z\d]+[a-z\d\-]?[a-z\d]+)?$")
 
-	var theme = settings.theme
+		url_match = req.path.match(url_regex)
 
-	app.use(theme.static_path)
-
-	app.set('views', theme.view_path)
-	app.set('view engine', 'html')
-
-	app.get(/^\/([a-z\d]+[a-z\d\-]?[a-z\d]+)?$/, function(req, res){
-		
-		var from = req.params[0]
-		var file_path = path.join(
-			settings.md_path, settings.default_page + settings.ext)
-		
-		if(from){
-			var file_path = path.join(settings.md_path, from + settings.ext)
+		if(url_match == null){
+			next()		
+			return
 		}
 
-		fs.readFile(file_path, function(err, data){
-			if(err == null){
-				res.render(theme.template, {
-					title: settings.title,
-					css: theme.css,
-					content: mmd.convert(data.toString())
-				})
-			} else {
-				res.send(404)
-			}		
-		})
-	})
+		var from = 	url_match[1]
+		if(from == undefined) {
+			from = settings.default_page
+		}
 
-	app.get('/*', function(req, res){
-	    res.send(404)
-	})
+		var file_path = path.join(settings.doc_path, from + settings.ext)
+		var toc_file_path = path.join(settings.doc_path, settings.toc_filename)
 
-
-	return app
+		if(req.isAuthenticated()) {
+			fs.readFile(file_path, function(err, data){
+				if(err == null){
+					parse_toc(toc_file_path, function(err, toc, meta){
+						if(err == null){
+							var toc_str = build_toc(toc, meta)
+							res.render(settings.theme.template, {
+								title: settings.title,
+								css: settings.theme.css,
+								content: mmd.convert(data.toString()),
+								toc: mmd.convert(toc_str)
+							})
+						} else {
+							console.log(err)
+							res.send(500)	
+						}
+					})
+				} else {
+					console.log(err)
+					next()
+				}
+			})
+		} else {
+			next()
+		}
+	}
 }
+
+var auth_middleware = function(auth_name) {
+	return function(req, res, next) {
+		req.authenticate([auth_name], function(err, authenticated) {
+    		if(err) {
+	        	console.log(err)
+	        	res.end()
+	      	}
+	      	else {
+	        	if(authenticated === undefined) {
+	          	}
+	          	else {
+	            	next()
+	          	}
+	      	}
+    	});
+  	}
+};
+
+exports.server = server
+exports.doc_md = doc_md
